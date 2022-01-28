b670ea957651388ee45bb248c78f59b059a95516
Benjamin Schulte <bs@golfgl.de>
2022-01-28 14:02:50 +0100

    Initial commit

---
+++ frontend/src/main/java/org/ergoplatform/example/client/application/ApplicationPresenter.java
@@ -0,0 +1,67 @@
+/*
+ * #%L
+ * GwtMaterial
+ * %%
+ * Copyright (C) 2015 - 2017 GwtMaterialDesign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.ergoplatform.example.client.application;
+
+import org.ergoplatform.example.client.resources.AppResources;
+import com.google.gwt.dom.client.StyleInjector;
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.proxy.Proxy;
+import com.gwtplatform.mvp.client.presenter.slots.NestedSlot;
+import gwt.material.design.addins.client.dark.AddinsDarkThemeLoader;
+import gwt.material.design.client.theme.dark.CoreDarkThemeLoader;
+import gwt.material.design.client.theme.dark.DarkThemeManager;
+
+public class ApplicationPresenter
+        extends Presenter<ApplicationPresenter.MyView, ApplicationPresenter.MyProxy> {
+
+    interface MyView extends View {
+    }
+
+    @ProxyStandard
+    interface MyProxy extends Proxy<ApplicationPresenter> {
+    }
+
+    public static final NestedSlot SLOT_MAIN = new NestedSlot();
+
+    @Inject
+    ApplicationPresenter(EventBus eventBus,
+            MyView view,
+            MyProxy proxy) {
+        super(eventBus, view, proxy, RevealType.Root);
+    }
+
+    @Override
+    protected void onBind() {
+        super.onBind();
+
+        // Dark Theme Mode
+        DarkThemeManager.get()
+            .register(new CoreDarkThemeLoader())
+            .register(new AddinsDarkThemeLoader())
+            .register(new AppDarkThemeLoader())
+            .load();
+
+        StyleInjector.inject(AppResources.INSTANCE.appCss().getText());
+    }
+}
---
+++ frontend/src/main/webapp/splash/splash.js
@@ -0,0 +1,30 @@
+var splashcontainer = document.createElement('div');
+splashcontainer.id = "splashscreen";
+
+var splashContent = document.createElement('div');
+
+var image = document.createElement("img");
+image.src = "https://gwtmaterialdesign.github.io/gmd-core-demo/launcher-icons/launcher2x.png";
+
+var title = document.createElement("span");
+title.className = "title";
+title.innerHTML = "ErgoPayExample";
+
+var description = document.createElement("span");
+description.className = "description";
+description.innerHTML = "Some description";
+
+var progress = document.createElement("div");
+progress.className = "progress";
+
+var progressFill = document.createElement("div");
+progressFill.className = "indeterminate";
+progress.append(progressFill);
+
+splashContent.append(image);
+splashContent.append(title);
+splashContent.append(description);
+splashContent.append(progress);
+
+splashcontainer.append(splashContent);
+document.body.appendChild(splashcontainer);
